f762909920d8e29e4fd3633fc0da31051ef77e47
theniswara <daniswara.mdr@proton.me>
2025-05-13 11:51:09 +0700

    Add login functionality with user authentication and error handling

---
+++ login.php
@@ -0,0 +1,101 @@
+<?php 
+
+  require 'functions.php';
+
+  if( isset($_POST["login"]) ) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+
+    // cek username
+    if( mysqli_num_rows($result) === 1 ) {
+
+      // cek password
+      $row = mysqli_fetch_assoc($result);
+      if( password_verify($password, $row['password'])) {
+        header("Location: index.php");
+        exit;
+      }
+    }
+
+    $error = true;
+
+
+  }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login Page</title>
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+
+    <!-- Boostrap -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT"
+      crossorigin="anonymous"
+    />
+    <!-- Boostrap -->
+
+  </head>
+  <body>
+
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <h1>Login Page</h1>
+
+          <form action="" method="post" autocomplete="off">
+            <div class="mb-3">
+              <label for="username" class="form-label">Username</label>
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                name="username"
+                required
+              />
+            </div>
+            <div class="mb-3">
+              <label for="password">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                name="password"
+                required
+              />
+            </div>
+            <?php if( isset($error) ) :?>
+            <div class="alert alert-danger mb-4" role="alert">
+              Username / Password salah!
+            </div>
+          <?php endif;?>
+
+            <button type="submit" name="login" class="btn btn-primary">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+
+    <!-- Boostrap -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+      crossorigin="anonymous"
+    ></script>
+    <!-- Boostrap -->
+  </body>
+</html>
